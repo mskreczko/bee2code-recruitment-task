0ec5a5db719c761b25cc5760924a7cc983e24720
mskreczko <mic.skreczko@gmail.com>
2023-03-17 16:51:44 +0100

    implemented tests for patient's controller and service

---
+++ medresearch/src/test/java/pl/mskreczko/medresearch/patient/PatientControllerTest.java
@@ -0,0 +1,127 @@
+package pl.mskreczko.medresearch.patient;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import pl.mskreczko.medresearch.domain.patient.Patient;
+import pl.mskreczko.medresearch.domain.patient.dto.PatientCreationDto;
+import pl.mskreczko.medresearch.domain.patient.dto.PatientDetailsDto;
+import pl.mskreczko.medresearch.domain.patient.service.PatientService;
+import pl.mskreczko.medresearch.domain.patient.web.PatientController;
+import pl.mskreczko.medresearch.exceptions.NoSuchEntityException;
+import pl.mskreczko.medresearch.exceptions.PatientAlreadyExistsException;
+
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(controllers = PatientController.class)
+public class PatientControllerTest {
+
+    @MockBean
+    PatientService patientService;
+
+    @Autowired
+    MockMvc mvc;
+
+    @Test
+    void getPatientById_returnsOk() throws Exception {
+        Mockito.when(patientService.getPatientById(UUID.fromString("53b1ab15-0663-46fb-b21b-199a7ff20e8e")))
+                .thenReturn(new Patient());
+
+        mvc.perform(MockMvcRequestBuilders
+                .get("/api/v1/patients/53b1ab15-0663-46fb-b21b-199a7ff20e8e"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void getPatientById_returnsNotFound() throws Exception {
+        Mockito.when(patientService.getPatientById(UUID.fromString("53b1ab15-0663-46fb-b21b-199a7ff20e8e")))
+                .thenThrow(NoSuchEntityException.class);
+
+        mvc.perform(MockMvcRequestBuilders
+                .get("/api/v1/patients/53b1ab15-0663-46fb-b21b-199a7ff20e8e"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void createNewPatient_returnsCreated() throws Exception {
+        Mockito.doNothing().when(patientService).createPatient(
+                new PatientCreationDto("John Doe", "test@test.com", "123456789")
+        );
+
+        mvc.perform(MockMvcRequestBuilders
+                .post("/api/v1/patients")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                {
+                    "name": "John Doe",
+                    "email": "test@test.com",
+                    "phoneNumber": "123456789"
+                }
+                """))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void createNewPatient_returnsConflict() throws Exception {
+        Mockito.doThrow(PatientAlreadyExistsException.class).when(patientService).createPatient(
+                new PatientCreationDto("John Doe", "test@test.com", "123456789")
+        );
+
+        mvc.perform(MockMvcRequestBuilders
+                .post("/api/v1/patients")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                {
+                    "name": "John Doe",
+                    "email": "test@test.com",
+                    "phoneNumber": "123456789"
+                }
+                """))
+                .andExpect(status().isConflict());
+    }
+
+    @Test
+    void updatePatientDetails_returnsOk() throws Exception {
+        Mockito.doNothing().when(patientService).updatePatientDetails(
+                UUID.fromString("53b1ab15-0663-46fb-b21b-199a7ff20e8e"),
+                new PatientDetailsDto("", "test@gmail.com", ""));
+
+        mvc.perform(MockMvcRequestBuilders
+                .patch("/api/v1/patients/53b1ab15-0663-46fb-b21b-199a7ff20e8e")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("""
+                {
+                    "name": "",
+                    "email": "test@gmail.com",
+                    "phoneNumber": ""
+                }
+                """))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void updatePatientDetails_returnsNotFound() throws Exception {
+        Mockito.doThrow(NoSuchEntityException.class).when(patientService).updatePatientDetails(
+                UUID.fromString("53b1ab15-0663-46fb-b21b-199a7ff20e8e"),
+                new PatientDetailsDto("", "test@gmail.com", ""));
+
+        mvc.perform(MockMvcRequestBuilders
+                        .patch("/api/v1/patients/53b1ab15-0663-46fb-b21b-199a7ff20e8e")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                {
+                    "name": "",
+                    "email": "test@gmail.com",
+                    "phoneNumber": ""
+                }
+                """))
+                .andExpect(status().isNotFound());
+    }
+}
---
+++ medresearch/src/test/java/pl/mskreczko/medresearch/patient/PatientServiceTest.java
@@ -0,0 +1,58 @@
+package pl.mskreczko.medresearch.patient;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import pl.mskreczko.medresearch.domain.patient.Patient;
+import pl.mskreczko.medresearch.domain.patient.PatientRepository;
+import pl.mskreczko.medresearch.domain.patient.dto.PatientCreationDto;
+import pl.mskreczko.medresearch.domain.patient.service.PatientService;
+import pl.mskreczko.medresearch.exceptions.NoSuchEntityException;
+import pl.mskreczko.medresearch.exceptions.PatientAlreadyExistsException;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@ExtendWith(MockitoExtension.class)
+public class PatientServiceTest {
+
+    @Mock
+    PatientRepository patientRepository;
+
+    @InjectMocks
+    PatientService patientService;
+
+    @Test
+    void getPatientById_returnsPatient() {
+        var mockPatient = new Patient();
+        mockPatient.setId(UUID.fromString("53b1ab15-0663-46fb-b21b-199a7ff20e8e"));
+        Mockito.when(patientRepository.findById(UUID.fromString("53b1ab15-0663-46fb-b21b-199a7ff20e8e")))
+                .thenReturn(Optional.of(mockPatient));
+
+        final var patient = patientService.getPatientById(UUID.fromString("53b1ab15-0663-46fb-b21b-199a7ff20e8e"));
+
+        Assertions.assertEquals(UUID.fromString("53b1ab15-0663-46fb-b21b-199a7ff20e8e"), patient.getId());
+    }
+
+    @Test
+    void getPatientById_throwsException() {
+        Mockito.when(patientRepository.findById(UUID.fromString("53b1ab15-0663-46fb-b21b-199a7ff20e8e")))
+                .thenReturn(Optional.empty());
+
+        Assertions.assertThrows(NoSuchEntityException.class, () ->
+                patientService.getPatientById(UUID.fromString("53b1ab15-0663-46fb-b21b-199a7ff20e8e")));
+    }
+
+    @Test
+    void createPatient_throwsException() {
+        Mockito.when(patientRepository.findByEmail("test@test.com")).thenReturn(Optional.of(new Patient()));
+
+        Assertions.assertThrows(PatientAlreadyExistsException.class, () ->
+                patientService.createPatient(
+                        new PatientCreationDto("John Doe", "test@test.com", "123456789")));
+    }
+}
